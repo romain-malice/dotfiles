8fabda13976dd09be86223e73d450fde974fbd48
Romain Malice <romainmalice@gmail.com>
2025-07-20 17:04:22 +0200

    Nvim without plugins

--- nvim/.config/nvim/lua/plugins/typst.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'chomosuke/typst-preview.nvim',
-    version = '1.*',
-    opts = {
-        dependencies_bin = { ['tinymist'] = 'tinymist' }
-    }, -- lazy.nvim will implicitly calls `setup {}`
-}
