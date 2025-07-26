d5c548f012981161cbe5888251faa50f93efa54c
Romain Malice <romainmalice@gmail.com>
2025-07-27 00:36:57 +0200

    need to checkout

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,10 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            vim.lsp.enable("lua-language-server")
+            vim.lsp.enable("clangd")
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("tinymist")
+            vim.lsp.enable("bash_language_server")
         end,
     },
     {
